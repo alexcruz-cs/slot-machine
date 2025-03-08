21fd31de0f9bdf0e6e2241b8334477f37d24548d
Alex Cruz <alexcrzed@gmail.com>
2025-03-08 00:17:51 -0800

    even more fixes

--- app/slot-machine.tsx
+++ app/slot-machine.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { useState, useEffect } from "react"
-
 export default function SlotMachine()
 {
     return (
@@ -13,7 +11,6 @@ export default function SlotMachine()
                 <div className="reel"></div>
             </div>
 
-            <img className="fixed left-0 top-0 h-screen w-auto" src="slotreel.webp"/>
         </main>
 
     );
